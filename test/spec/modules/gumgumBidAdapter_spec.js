593aeb50136043bb2848e4bb60f5b15257a02097
anthony <anthony@gumgum.com>
2017-10-11 10:18:41 -0700

    ADSS-78 changed the bidRequest sent to ad server. put params in query, not in JSON payload.

--- test/spec/modules/gumgumBidAdapter_spec.js
+++ test/spec/modules/gumgumBidAdapter_spec.js
@@ -55,7 +55,7 @@ describe('gumgumAdapter', () => {
       {
         'bidder': 'gumgum',
         'params': {
-          'inSlot': '789'
+          'inSlot': '9'
         },
         'adUnitCode': 'adunit-code',
         'sizes': [[300, 250], [300, 600]],
@@ -65,12 +65,38 @@ describe('gumgumAdapter', () => {
       }
     ];
 
-    it('sends bid request to ENDPOINT via POST', () => {
+    it('sends bid request to ENDPOINT via GET', () => {
       const requests = spec.buildRequests(bidRequests);
       const request = requests[0];
-      expect(request.url).to.equal(ENDPOINT);
-      expect(request.method).to.equal('POST');
+      console.log('in testspec. bidRequest: ', request)
+      // expect(request.url).to.equal(ENDPOINT);
+      expect(request.method).to.equal('GET');
     });
+  })
+
+  describe('interpretResponse', () => {
+    let response = {
+      'ad': {
+        'id': 29593,
+        'width': 300,
+        'height': 250,
+        'ipd': 2000,
+        'markup': '<div style="width:298px;height:248px;background:#fff;display:block;border:1px solid #000;background:#fff url(https://c.gumgum.com/images/logo/all300.png) no-repeat scroll center center">\n<\/div>',
+        'ii': true,
+        'du': null,
+        'price': 0,
+        'zi': 0,
+        'impurl': 'http://g2.gumgum.com/ad/view/enc/hasR4Y3aDoKInNYRxCfOp-Uatqav0e1W53SJIj9O6H1wz_Fa2GDwdaH95ZNLv7mF-qw9O07jHy8pURLqGxiXoZtG6aNoB5VPCj0CL99KMqM6PqjltEdAYxCSOQDitoBjohKQICAWAZRKpz1SOa6uR-RyuIVXcvFYgEK_iQJhuGdH0z4z1lKDYiToe_acZaNBn37LP2n2Vn0',
+        'clsurl': 'http://g2.gumgum.com/ad/close?si=9&t=ggumtest&ab=29593&pv=aa8bbb65-427f-4689-8cee-e3eed0b89eec&pu=http%3A%2F%2Flocalhost%3A9876%2F%3Fid%3D27669636&lt=&to=&ts=1507676828211&er=0.00'
+      },
+      'pag': {
+        't': 'ggumtest',
+        'pvid': 'aa8bbb65-427f-4689-8cee-e3eed0b89eec',
+        'css': '#GGID ._gBadge._g.Badge {\n    z-index: 1000 !important;\n}\n\n/* DB-4066 */\nhtml { overflow-y: auto }',
+        'js': "G.infoFlag = 'japan';\n/* GG-18932 */\nG.samplerate = 100;\nG.loadObj('https://c.gumgum.com/libs/GG-18932.min.js',{type:'s',cb:false});\n/* AT-5020 */\nG.perfSampleRate = 100;\nconsole.log(\"environment\", env);"
+      },
+      'thms': 10000
+    }
   })
 
   // describe('interpretResponse', () => {
